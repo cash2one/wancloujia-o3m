ffe060704b2625e9fd7fff70a7dd8251b2c1b0ee
Chen Young <yangchen@jiaoyin.cm>
2013-10-30 10:53:49 +0800

    修改账号管理功能页面

--- mgr/management/commands/add_admin.py
+++ mgr/management/commands/add_admin.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 
 from django.core.management.base import BaseCommand
-from mgr.models import SuperUser
+from mgr.models import Administrator
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
         username = args[0]
         password = args[1] if len(args) > 1 else 'nameLR9969'
-        su = SuperUser(username=username)
-        su.set_password(password)
-        su.save()
+        admin = Administrator(username=username)
+        admin.set_password(password)
+        admin.is_staff = True
+        admin.save()
