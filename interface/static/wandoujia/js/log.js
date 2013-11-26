d5690363aa759e1b13ceae6059bf22984305f3fd
Jarvis <yuhan534@126.com>
2013-11-26 13:11:40 +0800

    微调表格当中某些字段的宽度

--- interface/static/wandoujia/js/log.js
+++ interface/static/wandoujia/js/log.js
@@ -1,2 +1,35 @@
-window.onXXX = function(data) {
-}
+/*
+(function(window) {
+    define(['Configuration'], function(CONFIG) {
+        console.log('Log - File loaded.');
+
+        var Log = function(data) {
+            var logSwitch = true;
+            if (logSwitch) {
+                data = data || {};
+
+                var url = 'wdj://window/log.json';
+                var datas = [];
+                var d;
+                for (d in data) {
+                    if (data.hasOwnProperty(d)) {
+                        datas.push(d + '=' + window.encodeURIComponent(data[d]));
+                    }
+                }
+                url += '?' + datas.join('&');
+
+                window.OneRingRequest('get', url, '', function(resp) {
+                    resp = JSON.parse(resp);
+                    if (resp.state_code === 200) {
+                        console.log('Log: ', url);
+                    }
+                });
+            }
+        };
+
+        return Log;
+    });
+}(this));
+
+
+*/
