818185ff97ccd911281a29a02e322d48e649da59
SongWei <xdudetermined@live.com>
2014-01-10 14:42:20 +0800

    fankui

--- suning/settings.py
+++ suning/settings.py
@@ -60,7 +60,7 @@ FROM_EMAIL = "491320274@qq.com"
 ALLOWED_HOSTS = ['suning.wandoujia.com']
 
 # Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# http://en.ikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
 TIME_ZONE = None
